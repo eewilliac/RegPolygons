16a7a18c33ac22dabd9fd4a104a87d7ccccea8d4
ewc <eewilliac@gmail.com>
2024-08-05 16:15:53 -0400

    improved generate_polygon_coords function

--- PolyCirc2.py
+++ PolyCirc2.py
@@ -1,16 +1,41 @@
 import numpy
 import math
 import pygame
+import sys
 
-def simple():
-    sides =4
-    radius =100
+width = 1000
+height = 500
+screen_color = screen_color = (0, 0, 1)
+line_color = (255, 0, 0)
+poly_coords = []
+
+def generate_polygon_coords():
+    sides = 4
+    radius = 100
     angle = 360/sides
-    for current_side in range(1,sides):
-        x = radius * math.cos(360/current_side)
-        y = radius * math.sin(360/current_side)
-        print("{},{}",x,y)
+    angle = angle * (math.pi / 180) #convert to radians
+    for current_side in range(0,sides):
+        x = 100 + radius * math.cos(angle*current_side)
+        y = 100 + radius * math.sin(angle*current_side)
+        coord = tuple([x,y])
+        poly_coords.append(coord)
+    poly_coords.append(poly_coords[0])
+    return poly_coords
+
+def main(poly_coords):
+    screen=pygame.display.set_mode((width,height))
+    screen.fill(screen_color)
+    #pygame.draw.line(screen,line_color, (60, 80), (130, 100))
+    pygame.draw.lines(screen,line_color,True,poly_coords)
+    pygame.display.flip()
+
+    while True:
+        for events in pygame.event.get():
+            if events.type == pygame.QUIT:
+                sys.exit(0)
+
 
 
 if __name__ == "__main__":
-    simple()
+    coords = generate_polygon_coords
+    main(coords)
